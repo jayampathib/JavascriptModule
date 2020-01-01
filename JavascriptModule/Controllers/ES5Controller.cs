e9638d55f27fe52d6d6261d228fa32a6bc7c8510
jayampathib <wmjb.weerasinghe@gmail.com>
2020-01-01 19:54:04 +0800

    0001 Add First Controller and view

---
+++ JavascriptModule/Controllers/ES5Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JavascriptModule.Controllers
+{
+    public class ES5Controller : Controller
+    {
+        // GET: ES5
+        public ActionResult IIFE()
+        {
+            return View();
+        }
+    }
+}
